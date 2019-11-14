06642f99c87de5cf8b57062581c67353756f93b1
HXC <464147377@qq.com>
2019-11-14 23:25:29 +0800

    Third day，it doesn’t work well.fine

--- PSO.py
+++ PSO.py
@@ -2,6 +2,8 @@ import random
 
 from particle import PT
 from fitnessfunction import fitness
+import math
+import numpy as np
 
 
 class PSO:
@@ -27,7 +29,7 @@ class PSO:
         return vlist
 
     def initparticle(self):  # 初始化一个粒子
-        particle = PT(psotest.initposition(self.Xdmax), psotest.initvelocity(self.Vdmax))
+        particle = PT(self.initposition(self.Xdmax), self.initvelocity(self.Vdmax))
         return particle
 
     def particlelist(self, num):  # 初始化一群粒子：位置，速度，适应度
@@ -85,16 +87,85 @@ class PSO:
             pi = self.copyparticle(p)
         return pi
 
+    def initoperator(self):  # 初始化算子，初始值为优化变量取值范围
+        olist = []
+        for i in range(self.M):
+            olist.append(2 * self.Xdmax)
+        return olist
+
+    def countFitX(self, plist):  # 计算子群适应度FitX
+        xsum = 0
+        for i in range(len(plist)):
+            xsum = xsum + plist[i].f
+        return xsum / len(plist)
+
+    def initFitXlist(self, plist):  # 计算当前的各种群适应度
+        Fitlist = []
+        scale = int(len(plist) / self.M)
+        for i in range(self.M):
+            Fitlist.append(self.countFitX(plist[scale * i:scale * (i + 1)]))
+        return Fitlist
+
+    def updateFitXlist(self, Fitlist, olist):  # 更新变异算子,按照论文公式,Fitlist为当前计算的各种群适应度,olist为算子列表
+        for i in range(self.M):
+            updatef = olist[i] * math.exp((self.M * Fitlist[i] - sum(Fitlist)) / (max(Fitlist)) - min(Fitlist))
+            if updatef > self.Xdmax / 4:
+                updatef = abs(self.Xdmax / 4 - updatef)
+            olist[i] = updatef
+        return olist
+
+    def initTdlist(self, td):  # 初始化阈值列表,为一个二元列表，阈值限制种群粒子的各维速度
+        Tdlist = [[td] * self.Dim for i in range(self.M)]
+        return Tdlist
+
+    def initgdlist(self):  # gdlist用于保存第d维发生逃逸总次数
+        gdlist = [[0] * self.Dim for i in range(self.M)]
+        return gdlist
+
+    def updategdtd(self, gdlist, tdlist, plist):  # 更新阈值列表
+        scale = int(len(plist) / self.M)
+        for i in range(self.M):  # 选择种群
+            choose = plist[scale * i:scale * (i + 1)]
+            for j in range(self.Dim):  # 选择维度
+                for k in range(scale):  # 种群中一个粒子
+                    if abs(choose[k].v[j]) < tdlist[i][j]:
+                        gdlist[i][j] = gdlist[i][j] + 1
+                if gdlist[i][j] > self.k1:
+                    gdlist[i][j] = 0
+                    tdlist[i][j] = tdlist[i][j] / self.k2
+
+    def escape(self, tdlist, plist, olist):  # 逃逸运动
+        scale = int(len(plist) / self.M)
+        for i in range(self.M):  # 选择种群
+            choose = plist[scale * i:scale * (i + 1)]
+            for j in range(self.Dim):  # 选择维度
+                for k in range(scale):  # 种群中一个粒子
+                    mflist = []  # 保存根据算子计算得到的适应值f
+                    if abs(choose[k].v[j]) < tdlist[i][j]:  # vid<Td则需要逃逸
+                        for l in range(self.M):
+                            newparticle = self.copyparticle(choose[k])
+                            v1 = np.random.randn() * olist[l]
+                            newparticle.x[j] = newparticle.x[j] + v1
+                            mflist.append(self.getfitness(newparticle))
+                            if min(mflist)==mflist[-1]:
+                                vl=v1
+                        minf = min(mflist)
+                        newparticle1 = self.copyparticle(choose[k])
+                        v2 = random.random() * self.Vdmax
+                        newparticle1.x[j] = newparticle1.x[j] + v2
+                        vmaxf = self.getfitness(newparticle1)
+                        if minf < vmaxf:
+                            choose[k].v[j] = vl
+                        else:
+                            choose[k].v[j] = v2
+
 
 if __name__ == "__main__":
-    psotest = PSO(fitness.Sf7, 3, 1, 1, 5, 0.8, 122, 2, 20, 100)
+    psotest = PSO(fitness.Sf7, 5, 5, 1, 1, 0.8, 2, 2, 20, 100)
     p = psotest.particlelist(20)
-    pi = psotest.initpilist(p)
-    a = psotest.getgbest(p)
-    b = psotest.getgbest(p[10:20])
-    pi[1] = psotest.updatev(pi[1], a, b)
-    pi[1] = psotest.updatex(pi[1])
-    s = fitness.Griewank(p[2])
-    s1 = fitness.Sf7(p[2])
-    print(s1)
-    print(p[2].x, p[2].v, p[2].f)
+    c = psotest.initoperator()
+    a = psotest.initTdlist(10)
+    b = psotest.initgdlist()
+    psotest.updategdtd(b, a, p)
+    psotest.escape(a, p, c)
+
--- PSOmain.py
+++ PSOmain.py
@@ -1,3 +1,29 @@
 from PSO import PSO
+from fitnessfunction import fitness
 
-def startpopulation()
+if __name__ == "__main__":
+    psotest = PSO(fitness.Sf7, 5, 5, 0.6, 1.4, 1.4, 5, 10, 20, 100)
+    # 初始化方法参数分别为fitfunc, Dim, M, w, c1, c2, k1, k2, Vdmax, Xdmax
+    p = psotest.particlelist(20)  # 初始化一个粒子为20的种群
+    gbest = []  # 记录全局最优解
+    gbest.append(psotest.getgbest(p))  # 初始化全局最好位置
+    pi = psotest.initpilist(p)  # 初始化记录粒子的最好位置
+    td = psotest.initTdlist(0.5)  # 初始化粒子群的速度阈值设为20
+    gd = psotest.initgdlist()  # 初始化记录粒子群逃逸次数的列表
+    olist = psotest.initoperator()  # 初始化算子
+    for i in range(40000):  # 迭代次数
+        for j in range(len(pi)):  # 更新粒子最好适应度
+            if p[j].f < pi[j].f:
+                pi[j] = psotest.copyparticle(p[j])
+        if psotest.getgbest(p).f < gbest[-1].f:
+            gbest.append(psotest.getgbest(p))  # 更新全局最好速度
+        for k in range(len(p)):  # 更新粒子速度
+            p[k] = psotest.updatev(p[k], pi[k], gbest[-1])  # 传入参数分别为当前粒子，当前粒子最好位置，全局粒子最好位置
+        psotest.escape(td, p, olist)  # 逃逸操作
+        for l in range(len(p)):  # 更新粒子位置和适应度
+            p[l] = psotest.updatex(p[l])
+        Fitlist = psotest.initFitXlist(p)
+        olist = psotest.updateFitXlist(Fitlist, olist)  # 更新算子
+        psotest.updategdtd(gd, td, p)  # 更新阈值
+    for m in range(len(gbest)):
+        print(gbest[m].f)
